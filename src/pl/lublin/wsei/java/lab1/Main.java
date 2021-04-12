b254537abfa7478ebb588ac523d0ed049e48747c
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-12 12:25:05 +0200

    Initial commit

---
+++ src/pl/lublin/wsei/java/lab1/Main.java
@@ -0,0 +1,11 @@
+package pl.lublin.wsei.java.lab1;
+
+public class Main {
+    /**
+     * Funkcja main naszej klasy
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
+}
