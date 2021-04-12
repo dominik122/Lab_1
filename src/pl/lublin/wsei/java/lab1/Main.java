4f069ee9a652cde5b3630af50a1e98e12e8b8f11
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-12 15:19:23 +0200

    Krok 7 funkcja System.out.println()

--- src/pl/lublin/wsei/java/lab1/Main.java
+++ src/pl/lublin/wsei/java/lab1/Main.java
@@ -6,8 +6,8 @@ public class Main {
      * @param args argumenty funkcji main
      */
     public static void main(String[] args){
-        System.out.print("Witamy");
-        System.out.print("w");
-        System.out.print("Javie");
+        System.out.println("Witamy");
+        System.out.println("w");
+        System.out.println("Javie");
     }
 }
