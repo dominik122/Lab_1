d3b2b76fee3bb2d83a64d62802cb9c4e2264cc91
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-12 17:22:29 +0200

    Krok 12 wydzielenie nowej metody capitalize(String) z fragmentu metody setName(String)

--- src/pl/lublin/wsei/java/lab1/mylib/Account.java
+++ src/pl/lublin/wsei/java/lab1/mylib/Account.java
@@ -6,15 +6,19 @@ public class Account {
     private String name;
 
     public void setName(String s){
+        name = capitalize(s);
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String capitalize(String s){
         String[] tokens = StringUtils.split(s);
 
         for (int i = 0; i < tokens.length; i++){
             tokens[i] = StringUtils.capitalize(tokens[i]);
         }
 
-        name = StringUtils.join(tokens, ' ');
-    }
-    public String getName(){
-        return name;
+        return StringUtils.join(tokens, ' ');
     }
 }
