e4b708508c11c2d249296fedab52f4c64efee801
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-12 12:35:39 +0200

    Kroki 1-4 Lab 1

--- src/pl/lublin/wsei/java/lab1/Main.java
+++ src/pl/lublin/wsei/java/lab1/Main.java
@@ -3,9 +3,9 @@ package pl.lublin.wsei.java.lab1;
 public class Main {
     /**
      * Funkcja main naszej klasy
-     * @param args
+     * @param args argumenty funkcji main
      */
     public static void main(String[] args){
-
+        System.out.println("Hello Java World !\n");
     }
 }
