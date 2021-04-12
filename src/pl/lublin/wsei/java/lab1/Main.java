2103c83671f461385be1dbd6832fb596eb169aa1
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-12 16:56:24 +0200

    Krok 11

--- src/pl/lublin/wsei/java/lab1/Main.java
+++ src/pl/lublin/wsei/java/lab1/Main.java
@@ -1,33 +1,31 @@
 package pl.lublin.wsei.java.lab1;
 
-import java.util.Scanner;
+import java.io.PrintStream;
+import java.util.Random;
 
 public class Main {
     /**
      * Funkcja main naszej klasy
+     *
      * @param args argumenty funkcji main
      */
-    public static void main(String[] args){
-        Scanner inp = new Scanner(System.in);
-        // int a, b;
-        do{
-            System.out.print("Podaj liczbe: ");
-            int a = inp.nextInt();
-            if (a == 0) break;
+    public static void main(String[] args) {
+        int[] arr = new int[30];
+        Random rand = new Random();
 
-            System.out.printf("dec=%s\nbin=%s\nhex=%s \n".toUpperCase(),
-                    a,
-                    leftPad(Integer.toBinaryString(a), '0', 4),
-                    leftPad(Integer.toHexString(a), '0', 4));
-        } while (true);
-    }
-
-    public static String leftPad(String aText, char aChar, int aWidth){
-        String result = aText;
-        for (int i = 0; i < aWidth; i += 1) {
-            result = aChar + result;
+        for (int i = 0; i < 30; i++) {
+            arr[i] = rand.nextInt();
         }
 
-        return result;
+        int mx = Integer.MIN_VALUE;
+        int mn = Integer.MAX_VALUE;
+        long avg = 0;
+        for (int l : arr) {
+            System.out.println(l);
+            if (l > mx) mx = l;
+            if (l < mn) mn = l;
+            avg += l;
+        }
+        System.out.printf("MIN = %d, MAX = %d, AVG = %f", mn, mx, (float) avg / arr.length);
     }
 }
